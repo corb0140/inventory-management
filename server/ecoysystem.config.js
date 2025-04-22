ab66674acdbe795f82f7cb971e69e3b25d1d91c8
corb0140 <corb0140@algonquinlive.com>
2025-04-22 14:32:51 -0400

    added gitignore to root, set PM2 and CIDR block

---
+++ server/ecoysystem.config.js
@@ -0,0 +1,18 @@
+/**
+ * PM2 Ecosystem Configuration File
+ * This file is used to configure the PM2 process manager for Node.js applications.
+ * It defines the application name, script to run, and environment variables.
+ * PM2 is a production process manager for Node.js applications with a built-in load balancer.
+ * It allows you to keep your applications alive forever, reload them without downtime, and facilitate common system admin tasks.
+ */
+module.export = {
+  apps: {
+    name: "inventory-management",
+    script: "npm",
+    args: "run dev",
+    env: {
+      NODE_ENV: "development",
+      ENV_VAR1: "environment variable",
+    },
+  },
+};
